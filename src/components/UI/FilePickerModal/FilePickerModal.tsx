12e2e18f632dc0a8cdd6faa43bafb30e9d758df1
Данил <dakwolfoxi@yandex.ru>
2024-05-28 11:02:48 +0300

    hot fix styles

--- src/components/UI/FilePickerModal/FilePickerModal.tsx
+++ src/components/UI/FilePickerModal/FilePickerModal.tsx
@@ -1,30 +1,43 @@
 import React, { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import loadImage from "blueimp-load-image";
 import "./styles.scss";
 import icons from "../../../assets/icons/icons";
 import UploadImageApiRequest from "../../../api/UploadImage/UploadImage";
 
 const FilePickerModal = ({ onClose, setFiles, type }: any) => {
   const uploadApi = new UploadImageApiRequest();
-  const onDrop = useCallback((acceptedFiles: any) => {
-    // Обработка выбранных файлов
 
-    // onClose()
-    if (acceptedFiles.length > 0) {
-      const formData = new FormData();
+  const onDrop = useCallback(
+    (acceptedFiles: any) => {
+      if (acceptedFiles.length > 0) {
+        const formData = new FormData();
 
-      acceptedFiles.forEach((file: any) => {
-        formData.append("files", file);
-      });
-
-      uploadApi.uploadImage(formData).then((resp) => {
-        if (resp.success) {
-          setFiles(resp.data);
-          onClose(); // Закрытие всплывающего окна после загрузки файлов
-        }
-      });
-    }
-  }, []);
+        acceptedFiles.forEach((file: any) => {
+          loadImage(
+            file,
+            //@ts-ignore
+            (canvas) => {
+              //@ts-ignore
+              canvas.toBlob((blob) => {
+                formData.append("files", blob, file.name);
+                if (formData.get("files")) {
+                  uploadApi.uploadImage(formData).then((resp) => {
+                    if (resp.success) {
+                      setFiles(resp.data);
+                      onClose(); // Закрытие всплывающего окна после загрузки файлов
+                    }
+                  });
+                }
+              }, file.type);
+            },
+            { orientation: true, canvas: true }
+          );
+        });
+      }
+    },
+    [uploadApi, setFiles, onClose]
+  );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
