947521965832805f99195ca5c1b4c7a8a301fbb2
Данил <dakwolfoxi@yandex.ru>
2024-05-28 15:06:47 +0300

    hot fix

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,8 +1,8 @@
 const apiConfig = {
-   baseUrl: 'https://belid.ru/api/',
-    baseUrlMedia: 'https://belid.ru/media/',
-//    baseUrl: 'http://51.250.126.124:8099/api/',
-//     baseUrlMedia: 'http://51.250.126:124:8099',
+//    baseUrl: 'https://belid.ru/api/',
+//     baseUrlMedia: 'https://belid.ru/media/',
+   baseUrl: 'http://51.250.126.124:8099/api/',
+    baseUrlMedia: 'http://51.250.126:124:8099/media/',
     apiKey: ''
 }
 
